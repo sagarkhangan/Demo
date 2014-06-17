e8b759684997c257397ddee56ce1e0e72d7ca4c0
sagar <compaq@compaq-PC>
2014-06-17 17:22:10 +0530

    1

---
+++ JPA_Demo/src/JPA_Demo.java
@@ -0,0 +1,28 @@
+import entity.Employee;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+public class JPA_Demo {
+public static void main(String args[]){
+Employee p = new Employee();
+p.setName("Hendro Steven");
+p.setId("Salatiga, Indonesia");
+p.setPh(62813909);
+
+JPA_Demo demo = new JPA_Demo();
+demo.persist(p);
+}
+
+public void persist(Object object) {
+EntityManagerFactory emf = javax.persistence.Persistence.createEntityManagerFactory("JPA_DemoPU");
+EntityManager em = emf.createEntityManager();
+em.getTransaction().begin();
+try {
+em.persist(object);
+em.getTransaction().commit();
+} catch (Exception e) {
+em.getTransaction().rollback();
+} finally {
+em.close();
+}
+}
+}
