d0fae06d5b00fd8178cafd3a686e610655776683
sagar <compaq@compaq-PC>
2014-06-17 17:59:06 +0530

    2

--- JPA_Demo/src/JPA_Demo.java
+++ JPA_Demo/src/JPA_Demo.java
@@ -1,15 +1,29 @@
 import entity.Employee;
+import java.util.Scanner;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 public class JPA_Demo {
 public static void main(String args[]){
+    
+    Scanner sc;   
+    sc = new Scanner(System.in);
+    String name,id;
+    int ph;
+    System.out.println("enter name ");
+    name=sc.nextLine();
+    System.out.println("enter id ");
+    id=sc.nextLine();
+    System.out.println("enter phone ");
+    ph=sc.nextInt();
+    
 Employee p = new Employee();
-p.setName("XYZ");
-p.setId("ABC!@#ABC123");
-p.setPh(62813909);
+p.setName(name);
+p.setId(id);
+p.setPh(ph);
 
 JPA_Demo demo = new JPA_Demo();
 demo.persist(p);
+
 }
 
 public void persist(Object object) {
